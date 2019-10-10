b3035d248e4ed6fec1578e5614843ff5dbf157ff
Sinclair Seo <sinclairseo@gmail.com>
2019-10-10 18:16:55 +0900

    2.21 Home Controller

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const SEARCH = "/search";
 // Users
 const USERS = "/users";
 const USER_DETAIL = "/:id";
-const EDIT_PROFILE = "/edit";
+const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
 // Videos
